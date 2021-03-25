82cd294e69926abcf3a9119ba6cb540bba073d0a
andres2000gb <andres2000gb@gmail.com>
2021-03-25 09:10:46 +0100

    loading

--- Guinote/app/src/main/java/com/app/guinote/Login.java
+++ Guinote/app/src/main/java/com/app/guinote/Login.java
@@ -15,6 +15,9 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
 
 public class Login extends Fragment {
     private static long mLastClickTime = 0;
@@ -29,10 +32,14 @@ public class Login extends Fragment {
                 container, false);
         final LottieAnimationView animacion = (LottieAnimationView) getActivity().findViewById(R.id.animation_carga_reg2);
         final RelativeLayout cuerpo = (RelativeLayout) getActivity().findViewById(R.id.pantalla_prin);
+        final TextInputEditText nombre= (TextInputEditText) view.findViewById(R.id.username);
+        final TextInputEditText passwd= (TextInputEditText) view.findViewById(R.id.passwd);
         Button loggear = (Button) view.findViewById(R.id.login_boton);
         loggear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
+                nombre.setFocusable(false);
+                passwd.setFocusable(false);
                 if (SystemClock.elapsedRealtime() - mLastClickTime < 2000) {
                     return;
                 }
@@ -50,6 +57,9 @@ public class Login extends Fragment {
                         animacion.setVisibility(View.INVISIBLE);
                         cuerpo.getBackground().setAlpha(255);
                         Intent intent = new Intent(v.getContext(),Pantalla_app.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.putExtra("EXIT",true);
                         startActivity(intent);
                     }
                 }.start();
