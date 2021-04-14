19f18d42144e93d46e81c86802ba5c0fab3508f9
andres2000gb <andres2000gb@gmail.com>
2021-04-14 16:47:45 +0200

    registtro

--- Guinote/app/src/main/java/com/app/guinote/Registro.java
+++ Guinote/app/src/main/java/com/app/guinote/Registro.java
@@ -9,8 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Registro extends Fragment {
 
@@ -21,13 +24,22 @@ public class Registro extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_registro,
+        final View view = inflater.inflate(R.layout.activity_registro,
                 container, false);
         Button registro = view.findViewById(R.id.registro_boton);
+
         registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TextInputLayout username= (TextInputLayout) view.findViewById(R.id.usernameRegister);
+                TextInputLayout email = (TextInputLayout) view.findViewById(R.id.emailRegister);
+                TextInputLayout passwd= (TextInputLayout) view.findViewById(R.id.passwdRegister);
                 Intent intent = new Intent(getActivity(),carga_registro.class);
+                Bundle b = new Bundle();
+                b.putString("username", username.getEditText().getText().toString()); //Your id
+                b.putString("email", email.getEditText().getText().toString()); //Your id
+                b.putString("passwd", passwd.getEditText().getText().toString()); //Your id
+                intent.putExtras(b); //Put your id to your next Intent
                 startActivity(intent);
             }
         });
--- Guinote/app/src/main/java/com/app/guinote/carga_registro.java
+++ Guinote/app/src/main/java/com/app/guinote/carga_registro.java
@@ -5,10 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -23,6 +33,37 @@ public class carga_registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final LottieAnimationView animacion = (LottieAnimationView) findViewById(R.id.animation_carga_reg);
+        Bundle b=getIntent().getExtras();
+        String name=b.getString("username");
+        String email=b.getString("email");
+        String password=b.getString("passwd");
+
+        String postUrl = "http://192.168.1.33:8080/api/auth/signup";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("username", name);
+            postData.put("email", email);
+            postData.put("password", password);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
         new CountDownTimer(3000, 1000) {
 
             public void onTick(long millisUntilFinished) {
