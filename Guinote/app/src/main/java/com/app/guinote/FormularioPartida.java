ea0d45c73c58623dce4bab175809b4002ba55eb6
Diegol0010 <diegoguiserra@gmail.com>
2021-05-21 11:43:41 +0200

    Z

--- Guinote/app/src/main/java/com/app/guinote/FormularioPartida.java
+++ Guinote/app/src/main/java/com/app/guinote/FormularioPartida.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
